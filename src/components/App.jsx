accf1836b3c547472bdd785cda1c4bfc04bd40da
marrakeshn <nesveteymark@gmail.com>
2023-04-05 23:08:18 +0300

    done task 4
    Create TransactionHistory component, add styles

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,18 +9,24 @@ import data from '../configs/data';
 import { FriendList } from './FriendList';
 import friends from '../configs/friends';
 
+import { TransactionHistory } from './TransactionHistory';
+import transactions from '../configs/transactions';
+
 export const App = () => {
   return (
     <Container>
-      <Profile
-          username={user.username}
-          tag={user.tag}
-          location={user.location}
-          avatar={user.avatar}
-          stats={user.stats}
-      />
-      <Statistics title="Upload stats" stats={data} />
-      <FriendList friends={friends} />
+      <div>
+        <Profile
+            username={user.username}
+            tag={user.tag}
+            location={user.location}
+            avatar={user.avatar}
+            stats={user.stats}
+        />
+        <Statistics title="Upload stats" stats={data} />
+        <FriendList friends={friends} />
+      </div>
+      <TransactionHistory items={transactions} />
     </Container>
   );
 };
