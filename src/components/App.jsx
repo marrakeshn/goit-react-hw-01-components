750c6512a6f0567a8212ffbed29e36e64e896622
marrakeshn <nesveteymark@gmail.com>
2023-04-05 16:18:05 +0300

    done task 2
    create stats card

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,20 @@
 import { Profile } from './Profile';
 import user from '../configs/user';
 
+import { Statistics } from './Statistics';
+import data from '../configs/data';
+
 export const App = () => {
   return (
-    <Profile 
-    username={user.username}
-    tag={user.tag}
-    location={user.location}
-    avatar={user.avatar}
-    stats={user.stats}
-    />
-    
+    <div>
+      <Profile
+          username={user.username}
+          tag={user.tag}
+          location={user.location}
+          avatar={user.avatar}
+          stats={user.stats}
+      />
+      <Statistics title="Upload stats" stats={data} />
+    </div>
   );
 };
