fca6fb116108beeb09f575fe75e229dc312ae2bd
marrakeshn <nesveteymark@gmail.com>
2023-04-10 20:56:47 +0300

    Update FriendListItem.jsx
    fix FriendListItem.propTypes

--- src/components/FriendList/FriendListItem/FriendListItem.jsx
+++ src/components/FriendList/FriendListItem/FriendListItem.jsx
@@ -12,9 +12,7 @@ export const FriendListItem = ({ avatar, name, isOnline }) => {
 };
 
 FriendListItem.propTypes = {
-  friend: PropTypes.shape({
     avatar: PropTypes.string.isRequired,
     name: PropTypes.string.isRequired,
-    isOnline: PropTypes.bool.isRequired,
-  }).isRequired,
+    isOnline: PropTypes.bool.isRequired
 };
